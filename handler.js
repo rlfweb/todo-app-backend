297c3bbf5c7fd01308b7c78eecf5a0426b8f0077
Rebecca French <rlfweb8@gmail.com>
2020-02-08 13:37:31 +0000

    handler.js working for GET array of (hardcoded) tasks

--- handler.js
+++ handler.js
@@ -2,11 +2,33 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
-app.get('/tasks', function (req, res) {
-  res.send({
-    message: 'Your API works',
+
+
+
+  // update this to send back an array of tasks - remember the codes we hard-coded in react state - maybe just paste that in. Put the array in here and then Postman will display them / respond with an array of tasks. Request / Response. It'll still be res.json. 
+  // It's the contents of this that will change:
+  // ({
+  //   message: 'Your API works',
+  // })
+
+  app.get('/tasks/', function(req, res) {
+    res.json({
+      state: {
+        workTasks: [
+          { id: 1, description: "Phone chat with Sarah", completed: false },
+          { id: 2, description: "Learn JS", completed: false },
+          { id: 3, description: "View webinar", completed: false },
+          { id: 4, description: "Spray mount for cards", completed: false }
+        ],
+        homeTasks: [
+          { id: 1, description: "Reply to Viv", completed: false },
+          { id: 2, description: "Get bike serviced", completed: false },
+          { id: 3, description: "Take back Sloth", completed: false },
+          { id: 4, description: "Food for lunch", completed: false }
+        ],
+      }
+    });
   });
-});
 
 // we're saying, when someone sends a get request to /tasks, we want this function to fire. Request. Response. The response is what goes back to the client. 
 
