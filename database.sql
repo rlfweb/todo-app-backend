d6adcbe3a4db30af27980209edf48dcbabfb9cef
Rebecca French <rlfweb8@gmail.com>
2020-02-10 09:52:19 +0000

    database updated with enum data type for category

--- database.sql
+++ database.sql
@@ -26,7 +26,7 @@ CREATE TABLE `tasks` (
   `taskId` bigint(20) NOT NULL,
   `taskDescription` varchar(100) DEFAULT NULL,
   `completed` tinyint(1) DEFAULT NULL,
-  `category` varchar(4) DEFAULT NULL,
+  `category` enum('WORK','HOME') DEFAULT NULL,
   `userId` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`taskId`),
   KEY `userId` (`userId`),
@@ -43,3 +43,38 @@ LOCK TABLES `tasks` WRITE;
 INSERT INTO `tasks` VALUES (1,'Phone chat with Sarah',0,'WORK',1),(2,'Learn JS',0,'WORK',1),(3,'View webinar',0,'WORK',1),(4,'Spray mount for cards',0,'WORK',1),(5,'Reply to VP and Felix',0,'HOME',1),(6,'Get bike serviced',0,'HOME',1),(7,'Take back Sloth',0,'HOME',1),(8,'Food for lunch',0,'HOME',1);
 /*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Susan Jones'),(2,'Richard Johns');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-10  9:50:30
