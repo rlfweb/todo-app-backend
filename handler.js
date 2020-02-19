2802780659d357da2eb48bf15998945ba289af16
Rebecca French <rlfweb8@gmail.com>
2020-02-19 15:01:18 +0000

    External config for DB connection

--- handler.js
+++ handler.js
@@ -7,16 +7,16 @@ const uuidv4 = require('uuid/v4');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host     : 'XXXX',
-  user     : 'XXXX',
-  password : 'XXXX',
-  database : 'XXXX'
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_SCHEMA
 });
 
 // GET - RETRIEVING TASKS
   app.get('/tasks', function (req, res) {
 
-    connection.query('SELECT * FROM `tasks` WHERE `userID` = "1"', function (error, results, fields) {
+    connection.query('SELECT * FROM `tasks` WHERE `userId` = "1"', function (error, results, fields) {
 // error will be an Error if one occurred during the query
       if(error) {
         console.error("Your query had a problem with fetching tasks", error);
@@ -31,25 +31,25 @@ const connection = mysql.createConnection({
 // we're saying, when someone sends a get request to /tasks, we want this function to fire. Request. Response. The response is what goes back to the client. 
 
 
-// PUT - UPDATING / EDITING TASKS
+// // PUT - UPDATING / EDITING TASKS
   app.put('/tasks/:taskId', function (req, res) {
-    res.json({
-      message: 'Your PUT works - editing a task',
+      res.json({
+        message: 'Your PUT works - editing a task',
+      });
     });
-  });
 
 
-// POST - CREATING TASKS
-  app.post('/tasks', function (req, res) {
-    res.json({
-      message: 'Your POST works - making a new task',
+// // POST - CREATING TASKS
+    app.post('/tasks', function (req, res) {
+      res.json({
+        message: 'Your POST works - making a new task',
+      });
     });
-  });
 
 
-// DELETE - DELETING TASKS
+// // DELETE - DELETING TASKS
   app.delete('/tasks/:taskId', function (req, res) {
-    res.json({
+      res.json({
       message: 'Your DELETE works - deleting a task',
     });
   });
